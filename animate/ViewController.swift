bc6458a166fd6619b00ad34b34790a87a5444630
iamrishabhverma <46955354+iamrishabhverma@users.noreply.github.com>
2020-06-08 20:25:05 -0400

    Initial Commit

---
+++ animate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animate
+//
+//  Created by Rishabh Verma on 2020-06-08.
+//  Copyright © 2020 Rishabh Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
